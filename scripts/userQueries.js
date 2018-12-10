660c743ee119c8f7dd67d941618de610cd1d6af0
Clemo2604 <axelle.dru@stackerine.com>
2018-12-10 15:11:51 +0100

    External File

---
+++ scripts/userQueries.js
@@ -0,0 +1,9 @@
+// Useless so useful
+
+const lastname = prompt('Quel est votre nom ?');
+const firstname = (name = prompt('Quel est votre prénom ?'));
+const age = prompt('Quel est votre âge ?');
+
+console.log(
+  'Bonjour ' + firstname + ' ' + lastname + '. Vous avez ' + age + ' ans.'
+);
